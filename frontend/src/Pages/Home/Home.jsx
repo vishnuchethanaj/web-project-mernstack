354a177bb585db5ed6aa861b3791a4df5a7ccc24
vishnuchethana <2410030245@klh.edu.in>
2026-05-11 13:11:39 +0530

    initial setup

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Vishnuchethana's App 
           </Title>
 
           <Text
